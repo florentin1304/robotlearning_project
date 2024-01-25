dfd33dafe767d3f39d272de11889e9d2bdca1424
Florentin-Cristian Udrea <udrea.florentin00@gmail.com>
2024-01-25 23:43:03 +0100

    added load option in bayrn

--- bayrn.py
+++ bayrn.py
@@ -21,6 +21,7 @@ from stable_baselines3.common.monitor import Monitor
 from train import train
 from test import test
 import shutil
+import argparse
 
 #torch.set_default_dtype(torch.float64)
 
@@ -95,8 +96,7 @@ def get_next_points(train_x, train_y, train_y_var, best_y, bounds, n_points):
     return candidates
 
 
-
-def main():    
+def main(args):    
     #                m1,  m2,  m3, std1, std2, std3
     lower_bounds = [0.1, 0.1, 0.1,  0.1,  0.1, 0.1]
     upper_bounds = [10,   10,  10,    2,    2,   2]
@@ -104,51 +104,62 @@ def main():
     bounds = torch.tensor(bounds)
     book_keeping = []
 
-    NUM_INITIAL_POINTS = 5
-    NUM_ITERATIONS = 100 
+    if args.load == "":
+        # ======= GENERATE INITIAL TRAINING DATA =======   
+        print("="*25, "Generating initial points", "="*25)
+        train_x = np.zeros((args.n_init_points, len(bounds[0])))
+        for i in range(len(bounds[0])):
+            train_x[:, i] = np.random.uniform(bounds[0,i], bounds[1,i], args.n_init_points)
 
-    # ======= GENERATE INITIAL TRAINING DATA =======   
-    print("="*25, "Generating initial points", "="*25)
-    train_x = np.zeros((NUM_INITIAL_POINTS, len(bounds[0])))
-    for i in range(len(bounds[0])):
-        train_x[:, i] = np.random.uniform(bounds[0,i], bounds[1,i], NUM_INITIAL_POINTS)
-
-    train_y = []
-    train_y_var = []
-    
-
-    for iteration in range(NUM_INITIAL_POINTS):
-        print("="*70)
-        print("Pre BO point num = ", iteration)
-        iteration_checkpoint_filename = f"bayrn/checkpoints/initial_point_{iteration}.ai"
-
-        t_y, t_y_var, model = test_policy_params(train_x[iteration])
-        train_y.append(t_y)
-        train_y_var.append(t_y_var)
+        train_y = []
+        train_y_var = []
         
-        model.save(os.path.join(os.getcwd(), iteration_checkpoint_filename))
-        if max(train_y) == train_y[-1]:
-            model.save(os.path.join(os.getcwd(),f"bayrn/best_model.ai"))
+        for iteration in range(args.n_init_points):
+            print("="*70)
+            print("Pre BO point num = ", iteration)
+            iteration_checkpoint_filename = f"bayrn/checkpoints/initial_point_{iteration}.ai"
 
-        book_keeping.append({
-            "params": list(train_x[-1]),
-            "outcome": t_y,
-            "outcome_var": t_y_var,
-            "saved_model": iteration_checkpoint_filename
-        })
-        with open("bayrn/data.json", "w") as datafile:
-            json.dump(book_keeping, datafile, indent=2)
+            t_y, t_y_var, model = test_policy_params(train_x[iteration])
+            train_y.append(t_y)
+            train_y_var.append(t_y_var)
             
+            model.save(os.path.join(os.getcwd(), iteration_checkpoint_filename))
+            if max(train_y) == train_y[-1]:
+                model.save(os.path.join(os.getcwd(),f"bayrn/best_model.ai"))
 
-    train_x = torch.tensor(train_x)
-    train_y = torch.tensor(train_y).reshape(-1,1)
-    train_y_var = torch.tensor(train_y_var).reshape(-1,1)
+            book_keeping.append({
+                "params": list(train_x[-1]),
+                "outcome": t_y,
+                "outcome_var": t_y_var,
+                "saved_model": iteration_checkpoint_filename
+            })
+            with open("bayrn/data.json", "w") as datafile:
+                json.dump(book_keeping, datafile, indent=2)
 
+        train_x = torch.tensor(train_x)
+        train_y = torch.tensor(train_y).reshape(-1,1)
+        train_y_var = torch.tensor(train_y_var).reshape(-1,1)
+    else:
+        # ======= LOAD TRAINING DATA FROM FILE =======   
+        train_x = []
+        train_y = []
+        train_y_var = []
+
+        with open(args.load) as myfile:
+            book_keeping = json.load(myfile)
+        
+        for iteration in book_keeping:
+            train_x.append(iteration["params"])
+            train_y.append(iteration["outcome"])
+            train_y_var.append(iteration["outcome_var"])
+
+        train_x = torch.tensor(train_x)
+        train_y = torch.tensor(train_y).reshape(-1,1)
+        train_y_var = torch.tensor(train_y_var).reshape(-1,1)
 
     # ======= BAYESTIAN OPTIMIZATION ITERATIONS =======
-    ### TODO: SAVE MODELS AND BEST MODEL + CSV FOR DETAILS
     print("="*25, "Start Bayesian Optimization", "="*25)
-    for iteration in range(1, NUM_ITERATIONS + 1):    
+    for iteration in range(1, args.n_iterations + 1):    
         print("="*70)
         print("Iteration number = ", iteration)
         t0 = time.time()
@@ -187,5 +198,16 @@ def main():
     
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--seed', default=0, type=int, help='Random seed')
+    parser.add_argument('--n_init_points', default=5, type=int, help='Number of initial points before BO process (if not loading)')
+    parser.add_argument('--n_iterations', default=30, type=int, help='Number BO iterations')
+    parser.add_argument("--load", type=str, default="", required=False, help="Path to JSON file to load old data")
+
+    parser.add_argument('--verbose', action='store_true', help='Verbose')
+    
+    args = parser.parse_args()
+    
+    
+    main(args)
     
