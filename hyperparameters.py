e4679c3dd1b93e1ccf46794fcec7b20809eb4b32
Florentin-Cristian Udrea <udrea.florentin00@gmail.com>
2024-02-03 23:35:03 +0100

    Added hyperparams and changed hyperparameters.py to make multiple runs

--- hyperparameters.py
+++ hyperparameters.py
@@ -17,68 +17,71 @@ from stable_baselines3.common.utils import get_linear_fn, constant_fn
 from utils import get_step_scheduler, get_exp_scheduler
 
 def main(args):
-    lrs = ["step_scheduler", "exp_scheduler", "lin_scheduler", 0.003, 0.0003, 0.00003]
+    tournaments = 5
+
+    lrs = ["lin_scheduler", "step_scheduler", "exp_scheduler", 0.003, 0.0003, 0.00003]
     gammas = [0.99, 0.90, 0.75, 0.5]
 
     for lr in lrs:
         for gamma in gammas:
-            set_seed(args.seed)
-            run_name = f"{args.algo}_{args.domain}_lr{lr}_gamma{gamma}"
-            proc_path = os.path.join(os.getcwd(), "hyperparams_tuning")    
-            log_dir = os.path.join(proc_path, "logs")
-            os.makedirs(log_dir, exist_ok=True)
+            for t in range(tournaments):
+                set_seed(args.seed + t)
+                run_name = f"{args.algo}_{args.domain}_lr{lr}_gamma{gamma}_{t}"
+                proc_path = os.path.join(os.getcwd(), "hyperparams_tuning")    
+                log_dir = os.path.join(proc_path, "logs")
+                os.makedirs(log_dir, exist_ok=True)
 
-            eval_env = Monitor(make_env(args))
-            env = Monitor(make_env(args), os.path.join(log_dir, run_name))
+                eval_env = Monitor(make_env(args))
+                env = Monitor(make_env(args), os.path.join(log_dir, run_name))
 
-            model_folder = "models"
-            model_folder = os.path.join(proc_path, model_folder)
-            os.makedirs(model_folder, exist_ok=True)
+                model_folder = "models"
+                model_folder = os.path.join(proc_path, model_folder)
+                os.makedirs(model_folder, exist_ok=True)
 
-            model_name = run_name
-            model_path = os.path.join(model_folder, model_name)
+                model_name = run_name
+                model_path = os.path.join(model_folder, model_name)
 
-            if lr == "lin_scheduler":
-                my_lr = get_linear_fn(0.003, 0.00003, 0.01)
-            elif lr == "step_scheduler":
-                my_lr = get_step_scheduler(0.003, 0.5, 0.2)      
-            elif lr == "exp_scheduler":
-                my_lr = get_exp_scheduler(0.003, 0.00003)
-            else:
-                my_lr = constant_fn(lr)
+                if lr == "lin_scheduler":
+                    my_lr = get_linear_fn(0.003, 0.00003, 0.01)
+                elif lr == "step_scheduler":
+                    my_lr = get_step_scheduler(0.003, 0.5, 0.2)      
+                elif lr == "exp_scheduler":
+                    my_lr = get_exp_scheduler(0.003, 0.00003)
+                else:
+                    my_lr = constant_fn(lr)
 
 
-            trained_model = train(env, \
-                        eval_env, \
-                        model_path=model_path, \
-                        algorithm=args.algo, \
+                trained_model = train(env, \
+                            eval_env, \
+                            model_path=model_path, \
+                            algorithm=args.algo, \
 
-                        learning_rate=my_lr, \
-                        gamma=gamma,\
+                            learning_rate=my_lr, \
+                            gamma=gamma,\
 
-                        total_timesteps=args.total_timesteps,\
-                        reward_threshold=args.reward_threshold if args.reward_threshold is not None else float('inf'),
-                        verbose=args.verbose)
+                            total_timesteps=args.total_timesteps,\
+                            reward_threshold=args.reward_threshold if args.reward_threshold is not None else float('inf'),
+                            verbose=args.verbose)
 
-            test_log_dir = os.path.join(proc_path, "test_logs")
-            os.makedirs(test_log_dir, exist_ok=True)
+                test_log_dir = os.path.join(proc_path, "test_logs")
+                os.makedirs(test_log_dir, exist_ok=True)
 
-            mean_reward, std_reward = test(trained_model, env, n_episodes=100)
+                mean_reward, std_reward = test(trained_model, env, n_episodes=100)
 
-            print("="*35)
-            log_file_name = run_name + ".txt"
-            log_file_path = os.path.join(test_log_dir, log_file_name)
-            file_contents = ""
-            file_contents += f"Model: {run_name}" + "\n"
-            file_contents += f"Domain tested on: {env.get_name() }" + "\n"
-            file_contents += f"Mean reward: {mean_reward}" + "\n"
-            file_contents += f"Std reward: {std_reward}"
-            
-            with open(log_file_path, "w") as f:
-                f.write(file_contents)
-            
-            print("="*30)
-            print(file_contents)
+                print("="*35)
+                log_file_name = run_name + ".txt"
+                log_file_path = os.path.join(test_log_dir, log_file_name)
+                file_contents = ""
+                file_contents += f"Model: {run_name}" + "\n"
+                file_contents += f"Domain tested on: {env.get_name()}" + "\n"
+                file_contents += f"Mean reward: {mean_reward}" + "\n"
+                file_contents += f"Std reward: {std_reward}"
+                
+                with open(log_file_path, "w") as f:
+                    f.write(file_contents)
+                
+                print("="*30)
+                print(file_contents)
 
 
 if __name__ == "__main__":
